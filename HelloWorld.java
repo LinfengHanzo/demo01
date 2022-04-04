39974af96297919adb006ede9ed5ce13baf32e2c
dragon <1545753312@qq.com>
2022-04-04 11:19:46 +0800

    master分支上对HelloWorld.java文件的提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld{
 		System.out.println("Hello World!");
 // 下面一行输出语句是在 hot-fix 分支上新增的 
 System.out.println("hot-fix: content");	}
+	// 下面一行输出语句是在 master 分支上新增的 
+	System.out.println("master: content");
 }
 
 
